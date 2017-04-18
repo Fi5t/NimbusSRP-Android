49ffb86d65359c516b326dd2cc60157d33daef1f
simbo1905 <simbo1905@60hertz.com>
2017-04-18 20:04:09 +0100

    Issue #20 Random number generation isn't to RFC 5054

---
+++ src/test/java/com/nimbusds/srp6/RandomNumberTest.java
@@ -0,0 +1,25 @@
+package com.nimbusds.srp6;
+
+import junit.framework.TestCase;
+
+import java.math.BigInteger;
+import java.security.SecureRandom;
+
+public class RandomNumberTest extends TestCase {
+
+    public void testRandoms() {
+        final SRP6Routines srp6Routines = new SRP6Routines();
+
+        final SecureRandom secureRandom = new SecureRandom();
+
+        final BigInteger N = new BigInteger("2").pow(512);
+
+        for( int i = 0; i < 1e5; i++){
+            BigInteger r = srp6Routines.generatePrivateValue(N, secureRandom);
+            //if( i % 1e4 == 0 ) System.out.println(r);
+            if( BigInteger.ZERO.compareTo(r) >= 0 ) fail("bad r<=0:"+r);
+            if( r.compareTo(N) >= 0 ) fail("bad r>=N"+r);
+        }
+
+    }
+}
